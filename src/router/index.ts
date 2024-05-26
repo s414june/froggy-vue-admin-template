bdc7715022281fbb87f7406db6946b18b2dcd2b1
s414june <s414june@gmail.com>
2024-05-26 18:13:14 +0800

    debug router component

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router"
 import AppLayout from "@/layout/AppLayout.vue"
+import AboutView from "@/views/AboutView.vue"
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,7 +17,7 @@ const router = createRouter({
 				{
 					path: "/",
 					name: "home",
-					component: import("../views/AboutView.vue"),
+					component: AboutView,
 					meta: {
 						title: "Home",
 					},
@@ -24,7 +25,7 @@ const router = createRouter({
 				{
 					path: "/about",
 					name: "about",
-					component: import("../views/AboutView.vue"),
+					component: AboutView,
 					meta: {
 						title: "About",
 					},
